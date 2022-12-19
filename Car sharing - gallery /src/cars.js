b80ad59a201529f40acc12ee5dca498c7d944ea7
Anatoly <anatolyavbme@gmail.com>
2022-12-19 20:01:23 +0200

    HW#9 adds events listener

--- Car sharing - gallery /src/cars.js
+++ Car sharing - gallery /src/cars.js
@@ -1,23 +1,32 @@
-const detailsImage = document.querySelector(".lambo-class");
-const detailsTitle = document.querySelector(".anotation");
-const anchorElements = document.querySelectorAll(".thumbnails-anchor");
+const detailsImage = document.querySelector(".details-image");
+const detailsTitle = document.querySelector(".details-title");
+const detailsContainer = document.querySelector(".details-container");
+const anchorElements = document.querySelectorAll(".thumbnails-anchor")
 const mainElement = document.querySelector(".main-class");
-const HIDDEN ="hidden";
-function showDetail(){
-mainElement.classList.remove(HIDDEN);
+const hideButtonElement = document.getElementById("hide-button");
+const HIDDEN = "hidden";
+const IS_POINT = "is-point";
+function showDetails() {
+    mainElement.classList.remove(HIDDEN);
+    detailsContainer.classList.add(IS_POINT);
+    setTimeout(function() {
+        detailsContainer.classList.remove(IS_POINT);
+    })
+
 }
 function hideDetails() {
- mainElement.classList.add(HIDDEN);
+    mainElement.classList.add(HIDDEN);
 }
-function setDetails(anchor){
-    showDetail();
-    detailsImage.src = anchor.getAttribute("data-details-image");
+function setDetails(anchor) {
+    const dataImage = anchor.getAttribute("data-details-image");
+    detailsImage.src = dataImage;
+    showDetails();
     detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
 }
-for(let i = 0; i < anchorElements.length; i++)
-{
+for (let i = 0; i < anchorElements.length; i++) {
     anchorElements[i].addEventListener("click",function(){
         setDetails(anchorElements[i]);
-    })
+    } )
 }
 hideButtonElement.addEventListener("click",hideDetails);
+
